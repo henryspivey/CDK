f179ec52669ef59ec487ef3c7224bd052cc4f092
Henry Spivey <henryspivey2@gmail.com>
2023-03-20 23:42:08 -0700

    step 2 -- added gateway

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
-import { aws_lambda as lambda } from 'aws-cdk-lib';
+
+import { aws_lambda as lambda, aws_apigateway as apigw } from 'aws-cdk-lib';
+
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -8,6 +10,10 @@ export class CdkWorkshopStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: "hello.handler",
+    })
+
+    new apigw.LambdaRestApi(this, "helloRoute", {
+      handler: hello
     })
   }
 }
